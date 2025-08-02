acd93b14bdafbdc70772be60a53c280ded5721d9
Colton Stotler <coltonstotler0@gmail.com>
2025-08-02 18:06:33 -0400

    Created login form.

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,6 +1,23 @@
 from myapp import app
+from flask import render_template, flash, redirect, url_for
+from myapp.forms import LoginForm
+
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+
+@app.route('/home')
+def home():
+    user = {'username': 'Miguel'}
+    return render_template('home.html', title='Home', user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me{}'.format(
+            form.email.data, form.remember_me.data
+        ))
+        return redirect(url_for('home'))
+    
+    return render_template('login.html', title='Log In', form=form)
